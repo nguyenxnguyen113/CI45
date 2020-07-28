c8736bf29b41b7628e9247df931f7cbe617394b5
nguyenxnguyen113 <65146661+nguyenxnguyen113@users.noreply.github.com>
2020-07-28 17:26:45 +0700

    Update index.js
    import firebase

--- Web-chat/js/index.js
+++ Web-chat/js/index.js
@@ -1,4 +1,17 @@
 const init = () => {
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyB-GTJb8eYa945i34u-rOvedSTYaQUIzqg",
+        authDomain: "chat-6bc04.firebaseapp.com",
+        databaseURL: "https://chat-6bc04.firebaseio.com",
+        projectId: "chat-6bc04",
+        storageBucket: "chat-6bc04.appspot.com",
+        messagingSenderId: "71368848088",
+        appId: "1:71368848088:web:c3182f74002be640a19dad"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    console.log(firebase.app().name)
     console.log('Windown loaded')
     view.setActiveScreen('registerScreen')
 }
